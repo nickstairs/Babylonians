04b2bcb6c59df259a57534504e2a827b7e03389a
nickstairs <nickmalone2002@gmail.com>
2021-04-19 16:37:36 -0500

    test

--- Learning_Python_Test_Enviorment.py
+++ Learning_Python_Test_Enviorment.py
@@ -11,3 +11,11 @@ for x in str:
         count+=1
 
 print (count)
+
+#axisY =[a,b,c,d,e,f,g,h]
+#['a','b','c','d','e','f','g','h']
+#print(str(axisY[2]))
+list=[1]
+while len(list)<9:
+    print(list)
+    list.append+=1
